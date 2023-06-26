7619e50a2992945249dd914f0cda00a33dabcb37
Kyale_Peter <103901691+peter-mwau@users.noreply.github.com>
2023-06-27 00:09:54 +0300

    Add files via upload

---
+++ chatgpt_gradio.py
@@ -0,0 +1,27 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages=[{"role": "system", "content": "You are a Programming Experst"}]
+
+def cCustom_GPT(msg):
+    messages.append({"role": "user", "content": msg})
+    completion  = openai.ChatCompletion.create(model='gpt-3.5-turbo',  messages=messages)
+    response = completion.choices[0].message.content
+    messages.append({"role": "system", "content": response})
+    return response
+
+demo = gradio.Interface(fn=cCustom_GPT, inputs="text", outputs="text", title="Coder Chatbot")
+
+demo.launch(share=True)
+
+
+
+
+
+
+
+
+
+
